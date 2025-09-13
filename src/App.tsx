727c6a3171cbd02fc88df03fcf981b032a08fe37
kingdamie <damyridwan@gmail.com>
2025-09-13 15:57:22 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>
+        QR Code Generator
+      </h1>
+    </>
+  )
+}
+
+export default App
